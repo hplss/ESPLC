0149a1f01e998c1979a99ddf57be6a4d6527df3e
CosmicStrife <45959708+CosmicStrife@users.noreply.github.com>
2020-10-31 20:12:23 -0500

    Finished resolving merge conflicts

--- src/PLC/OBJECTS/obj_var.cpp
+++ src/PLC/OBJECTS/obj_var.cpp
@@ -53,3 +53,44 @@ void Ladder_VAR::setLineState(bool &state, bool bNot)
 
     Ladder_OBJ_Logical::setLineState(state, bNot); 
 }
+
+void Ladder_VAR::setValue( const String &str )
+{
+    if ( getType() == OBJ_TYPE::TYPE_VAR_FLOAT )
+        setValue( str.toDouble() );
+    else
+        setValue( static_cast<int64_t>(strtoll(str.c_str(), NULL, 10)) );
+}
+
+String Ladder_VAR::getValueStr()
+{
+    String value;
+    switch(getType()) //get the correct value from the variable object
+    {
+        case OBJ_TYPE::TYPE_VAR_USHORT:
+            value = getValue<uint16_t>();
+        break;
+        case OBJ_TYPE::TYPE_VAR_UINT:
+            value = getValue<uint_fast32_t>();
+        break;
+        case OBJ_TYPE::TYPE_VAR_INT:
+            value = getValue<int_fast32_t>();
+        break;
+        case OBJ_TYPE::TYPE_VAR_ULONG:
+            value = intToStr(getValue<uint64_t>());
+        break;
+        case OBJ_TYPE::TYPE_VAR_LONG:
+            value = intToStr(getValue<int64_t>());
+        break;
+        case OBJ_TYPE::TYPE_VAR_FLOAT:
+            value = getValue<double>();
+        break;
+        case OBJ_TYPE::TYPE_VAR_BOOL:
+            value = getValue<bool>();
+        break;
+        default: //default case
+        break;
+    }
+
+    return value;
+}
--- src/PLC/OBJECTS/obj_var.h
+++ src/PLC/OBJECTS/obj_var.h
@@ -50,44 +50,67 @@ class Ladder_VAR : public Ladder_OBJ_Logical
 					{
 							if ( b_usesPtr )
 									return static_cast<T>(*values.b.val_ptr);
+							else
+									return static_cast<T>(values.b.val);
 					}
 					break;
 					case OBJ_TYPE::TYPE_VAR_FLOAT:
 					{
 							if ( b_usesPtr )
 									return static_cast<T>(*values.d.val_ptr);
+							else
+									return static_cast<T>(values.d.val);
+					}
+					break;
+					case OBJ_TYPE::TYPE_VAR_USHORT:
+					{
+							if ( b_usesPtr )
+									return static_cast<T>(*values.us.val_ptr);
+							else
+									return static_cast<T>(values.us.val);
 					}
 					break;
 					case OBJ_TYPE::TYPE_VAR_INT:
 					{
 							if ( b_usesPtr )
 									return static_cast<T>(*values.i.val_ptr);
-					}
-					break;
-					case OBJ_TYPE::TYPE_VAR_LONG:
-					{
-							if ( b_usesPtr )
-									return static_cast<T>(*values.l.val_ptr);
-					}
-					break;
-					case OBJ_TYPE::TYPE_VAR_ULONG:
-					{
-							if ( b_usesPtr )
-									return static_cast<T>(*values.ul.val_ptr);
+							else
+									return static_cast<T>(values.i.val);
 					}
 					break;
 					case OBJ_TYPE::TYPE_VAR_UINT: 
 					{
 							if ( b_usesPtr )
 									return static_cast<T>(*values.ui.val_ptr);
+							else
+									return static_cast<T>(values.ui.val);
 					}
 					break;
+					case OBJ_TYPE::TYPE_VAR_LONG:
+					{
+							if ( b_usesPtr )
+									return static_cast<T>(*values.l.val_ptr);
+							else
+									return static_cast<T>(values.l.val);
+					}
+					break;
+					case OBJ_TYPE::TYPE_VAR_ULONG:
+					{
+							if ( b_usesPtr )
+									return static_cast<T>(*values.ul.val_ptr);
+							else
+									return static_cast<T>(values.ul.val);
+					}
+					break;
+					default:
+					break;
 			}
+
 			return static_cast<T>(0);
 	}
 
 	template <typename T>
-	void setValue( const T val )
+	void setValue( const T val ) //Doesn't support String type
 	{
 			switch(getType())
 			{
@@ -95,37 +118,59 @@ class Ladder_VAR : public Ladder_OBJ_Logical
 					{
 							if ( b_usesPtr )
 									*values.b.val_ptr = static_cast<bool>(val);
+							else
+									values.b.val = static_cast<bool>(val);
 					}
 					break;
 					case OBJ_TYPE::TYPE_VAR_FLOAT:
 					{
 							if ( b_usesPtr )
 									*values.d.val_ptr = static_cast<double>(val);
+							else
+									values.d.val = static_cast<double>(val);
+					}
+					break;
+					case OBJ_TYPE::TYPE_VAR_USHORT:
+					{
+							if ( b_usesPtr )
+									*values.us.val_ptr = static_cast<uint16_t>(val);
+							else
+									values.us.val = static_cast<uint16_t>(val);
 					}
 					break;
 					case OBJ_TYPE::TYPE_VAR_INT:
 					{
 							if ( b_usesPtr )
 									*values.i.val_ptr = static_cast<int_fast32_t>(val);
-					}
-					break;
-					case OBJ_TYPE::TYPE_VAR_LONG:
-					{
-							if ( b_usesPtr )
-									*values.l.val_ptr = static_cast<int64_t>(val);
-					}
-					break;
-					case OBJ_TYPE::TYPE_VAR_ULONG:
-					{
-							if ( b_usesPtr )
-									*values.ul.val_ptr = static_cast<uint64_t>(val);
+							else
+									values.i.val = static_cast<int_fast32_t>(val);
 					}
 					break;
 					case OBJ_TYPE::TYPE_VAR_UINT:
 					{
 							if ( b_usesPtr )
 									*values.ui.val_ptr = static_cast<uint_fast32_t>(val);
+							else
+									values.ui.val = static_cast<uint_fast32_t>(val);
 					}
+					break;
+					case OBJ_TYPE::TYPE_VAR_LONG:
+					{
+							if ( b_usesPtr )
+									*values.l.val_ptr = static_cast<int64_t>(val);
+							else
+									values.l.val = static_cast<int64_t>(val);
+					}
+					break;
+					case OBJ_TYPE::TYPE_VAR_ULONG:
+					{
+							if ( b_usesPtr )
+									*values.ul.val_ptr = static_cast<uint64_t>(val);
+							else
+									values.ul.val = static_cast<uint64_t>(val);
+					}
+					break;
+					default:
 					break;
 			}
 	}
